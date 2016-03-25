ab6395a92eb4b880330ba71f932539ab8646f6dc
benjamin <benjamin.le-neve@lengow>
2016-03-25 10:32:28 +0100

    Add export settings

--- app/code/community/Lengow/Connector/Model/System/Config/Source/Getattributes.php
+++ app/code/community/Lengow/Connector/Model/System/Config/Source/Getattributes.php
@@ -8,24 +8,57 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Lengow_Connector_Model_System_Config_Source_Getattributes extends Mage_Core_Model_Config_Data {
-
-    public function toOptionArray() {
+class Lengow_Connector_Model_System_Config_Source_Getattributes extends Mage_Core_Model_Config_Data
+{
+    /**
+     * Get option array for settings
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
         $attribute = Mage::getResourceModel('eav/entity_attribute_collection')
                            ->setEntityTypeFilter(Mage::getModel('catalog/product')
                            ->getResource()
                            ->getTypeId());
         $attributeArray = array();
-        $attributeArray[] = array('value' => 'none',
-                                  'label' => '',
-                          );        
+        $attributeArray[] = array('value' => 'none', 'label' => '');
         foreach ($attribute as $option) {
             $attributeArray[] = array(
                 'value' => $option->getAttributeCode(),
                 'label' => $option->getAttributeCode()
             );
-        }    
+        }
+        $this->setDefaultAttributes($attributeArray);
         return $attributeArray;
     }
 
+    /**
+     * Set default value with all attributes
+     *
+     * @param array $attributeArray
+     */
+    public function setDefaultAttributes($attributeArray = array())
+    {
+        if (strlen($code = Mage::getSingleton('adminhtml/config_data')->getStore())) {
+            $store_id = Mage::getModel('core/store')->load($code)->getId();
+        } else {
+            $code = 'default';
+            $store_id = 0;
+        }
+        if (is_null(Mage::getStoreConfig('lengow_export_options/advanced/export_attribute', $store_id))) {
+            $attribute_list = '';
+            foreach ($attributeArray as $attribute) {
+                if ($attribute['value'] != 'none') {
+                    $attribute_list.= $attribute['value'].',';
+                }
+            }
+            Mage::getModel('core/config')->saveConfig(
+                'lengow_export_options/advanced/export_attribute',
+                $attribute_list,
+                $code,
+                $store_id
+            );
+        }
+    }
 }
--- app/code/community/Lengow/Connector/Model/System/Config/Source/Types.php
+++ app/code/community/Lengow/Connector/Model/System/Config/Source/Types.php
@@ -8,29 +8,21 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-class Lengow_Connector_Model_System_Config_Source_Types extends Mage_Core_Model_Config_Data {
-
-    public function toOptionArray()  {
+class Lengow_Connector_Model_System_Config_Source_Types extends Mage_Core_Model_Config_Data
+{
+    /**
+     * Get option array for settings
+     *
+     * @return array
+     */
+    public function toOptionArray()
+    {
         return array(
-            array('value' => 'configurable',
-                'label' => Mage::helper('adminhtml')->__('Configurable')),
-            array('value' => 'simple',
-                'label' => Mage::helper('adminhtml')->__('Simple')),
-            array('value' => 'bundle',
-                'label' => Mage::helper('adminhtml')->__('Bundle')),
-            array('value' => 'grouped',
-                'label' => Mage::helper('adminhtml')->__('Grouped')),
-            array('value' => 'virtual',
-                'label' => Mage::helper('adminhtml')->__('Virtual')),
+            array('value' => 'configurable', 'label' => Mage::helper('adminhtml')->__('Configurable')),
+            array('value' => 'simple', 'label' => Mage::helper('adminhtml')->__('Simple')),
+            array('value' => 'bundle', 'label' => Mage::helper('adminhtml')->__('Bundle')),
+            array('value' => 'grouped', 'label' => Mage::helper('adminhtml')->__('Grouped')),
+            array('value' => 'virtual', 'label' => Mage::helper('adminhtml')->__('Virtual'))
         );
     }
-
-    public function toSelectArray() {
-        $select = array();
-        foreach($this->toOptionArray() as $option) {
-            $select[$option['value']] = $option['label'];
-        }
-        return $select;
-    }
-
 }
