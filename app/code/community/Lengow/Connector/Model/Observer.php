2dbad66a2ddbd1e9b67da2b5183046c998dd6eb1
pierre <pierre.basile@lengow.com>
2016-03-29 11:47:26 +0200

    add save configuration data on save

--- app/code/community/Lengow/Connector/Model/Observer.php
+++ app/code/community/Lengow/Connector/Model/Observer.php
@@ -11,6 +11,12 @@
 
 class Lengow_Connector_Model_Observer
 {
+    private $lengowOption = array(
+        'lengow_global_options',
+        'lengow_export_options',
+        'lengow_import_options'
+    );
+
     /**
      * Display Lengow Menu on demand
      */
@@ -19,6 +25,47 @@ class Lengow_Connector_Model_Observer
         $menu = Mage::getSingleton('admin/config')->getAdminhtmlConfig()->getNode('menu/lengowtab/children');
         foreach ($menu->children() as $childName => $child) {
             $menu->setNode($childName.'/disabled', '0');
+        }
+    }
+
+    /**
+     * Save Change on lengow data
+     * @param $obj
+     */
+    public function onAfterSave($obj)
+    {
+        $object = $obj->getEvent()->getObject();
+        if (is_a($object, 'Mage_Core_Model_Config_Data')) {
+            $pathExplode = explode("/", $object['path']);
+            if (isset($pathExplode[0]) && in_array($pathExplode[0], $this->lengowOption)) {
+                if ($object['scope'] == 'stores' || $object['scope'] == 'default') {
+                    $oldValue = Mage::getStoreConfig($object['path'], $object['scope_id']);
+                    if ($oldValue != $object['value']) {
+                        if ($object['scope'] == 'stores') {
+                            $message = Mage::helper('lengow_connector/translation')
+                                ->t(
+                                    'log.setting.setting_change_for_shop',
+                                    array(
+                                        'key' => $object['path'],
+                                        'old_value' => $oldValue,
+                                        'value' => $object['value'],
+                                        'shop_id' => $object['scope_id']
+                                    )
+                                );
+                        } else {
+                            $message = Mage::helper('lengow_connector/translation')->t(
+                                'log.setting.setting_change',
+                                array(
+                                    'key' => $object['path'],
+                                    'old_value' => $oldValue,
+                                    'value' => $object['value'],
+                                )
+                            );
+                        }
+                        Mage::helper('lengow_connector')->log('Config', $message);
+                    }
+                }
+            }
         }
     }
 }
