34dbff5cd0aa03b869f890cac09da73de196b8f8
benjamin <benjamin.le-neve@lengow>
2016-07-07 09:23:04 +0200

    Change checkStore fonction in product page

--- skin/adminhtml/default/default/lengow/js/home.js
+++ skin/adminhtml/default/default/lengow/js/home.js
@@ -23,16 +23,14 @@
                     }
                 });
             };
-
             if (sync_link) {
-                // synchronisation des boutiques, à modifier lorsque l'API sera disponible
+                // synchronisation des boutiques
                 // sync_iframe.src = 'http://cms.lengow.io/sync/';
-                sync_iframe.src = 'http://cms.lengow.int/sync/';
+                sync_iframe.src = 'http://cms.lengow.net/sync/';
             } else {
                 // sync_iframe.src = 'http://cms.lengow.io/';
-                sync_iframe.src = 'http://cms.lengow.int/';
+                sync_iframe.src = 'http://cms.lengow.net/';
             }
-
             $('#lengow-iframe').show();
         }
 
