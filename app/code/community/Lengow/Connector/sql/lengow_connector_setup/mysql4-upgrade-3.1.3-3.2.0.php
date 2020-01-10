b50abd8015dbfb65c8bc72e00cab505261a49994
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2020-01-10 14:18:45 +0100

    Declare new version 3.2.0

---
+++ app/code/community/Lengow/Connector/sql/lengow_connector_setup/mysql4-upgrade-3.1.3-3.2.0.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Copyright 2019 Lengow SAS
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ *
+ * @category    Lengow
+ * @package     Lengow_Connector
+ * @subpackage  sql
+ * @author      Team module <team-module@lengow.com>
+ * @copyright   2019 Lengow SAS
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
+$version = '3.2.0';
+/** @var Lengow_Connector_Helper_Config $configHelper */
+$configHelper = Mage::helper('lengow_connector/config');
+$installedVersion = $configHelper->get('installed_version');
+
+if (version_compare($installedVersion, $version, '<')) {
+    $configHelper->set('installed_version', $version);
+}
