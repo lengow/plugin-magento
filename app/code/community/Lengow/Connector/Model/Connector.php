0b49ef9b4ba5a3b1b1ef0f301f8edff67f07012f
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2020-01-14 17:00:47 +0100

    Delete use in connector class

--- app/code/community/Lengow/Connector/Model/Connector.php
+++ app/code/community/Lengow/Connector/Model/Connector.php
@@ -17,8 +17,6 @@
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
 
-use Lengow\Connector\Model\Exception as LengowException;
-
 /**
  * Model connector
  */
