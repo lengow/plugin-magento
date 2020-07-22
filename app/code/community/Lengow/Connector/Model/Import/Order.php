83c0558ebda2b9b669c1419dde53daf8f9d91c32
Benjamin Le Nevé <benjamin.le-neve@lengow.com>
2020-07-22 12:26:08 +0200

    BugFix: [import] Changing variable name into checkAndChangeMarketplaceName() function

--- app/code/community/Lengow/Connector/Model/Import/Order.php
+++ app/code/community/Lengow/Connector/Model/Import/Order.php
@@ -967,10 +967,10 @@ class Lengow_Connector_Model_Import_Order extends Mage_Core_Model_Abstract
         if (isset($results->error)) {
             return false;
         }
-        foreach ($results->results as $order) {
-            if ($order->getData('marketplace_lengow') !== (string)$order->marketplace) {
+        foreach ($results->results as $result) {
+            if ($order->getData('marketplace_lengow') !== (string)$result->marketplace) {
                 try {
-                    $order->setData('marketplace_lengow', (string)$order->marketplace);
+                    $order->setData('marketplace_lengow', (string)$result->marketplace);
                     $order->save();
                 } catch (Exception $e) {
                     continue;
